d1ceca13ddab54d6c045d05d112b026428cfcec2
anvar-muzaffarli <prof.anvar2013@gmail.com>
2021-10-19 20:23:18 +0400

    proyekt tamamlandi

--- oyun.js
+++ oyun.js
@@ -1,5 +1,8 @@
 const siyahi = ["kagiz.png", "scissors.png", "stone.png"]
 
+// siyahi[2]
+
+
 
 
 // DOM Document Object Model
@@ -19,12 +22,45 @@ das.onclick = seciminiEle
 
 
 function seciminiEle() {
+    const komputer = RandomSecim()
+    const secim = this.dataset.id
 
+    if(komputer == 0 && secim == 0) {
+        netice = "Yeniden cehd edin"
+    }
+    else if(komputer ==0 && secim ==1) {
+        netice = "Tebrikler!Kagiz qaycini kesdiyi ucun qazandin!"
+    }
+    else if (komputer ==0 && secim ==2) {
+        netice = "Uduzdun! Kagiz dashi bukur!"
+    }
+    else if (komputer ==1 && secim ==0) {
+        netice = "Uduzdun! Qayci kagizi kesir!"
+    }
+    else if (komputer ==1 && secim ==1) {
+        netice = "Yeniden cehd edin"
+    }
+    else if(komputer ==1 && secim ==2) {
+        netice = "Tebrikler qazandin dash qaycini sindirir!"
+    }
+    else if(komputer ==2 && secim ==0) {
+        netice = "Tebrikler kagiz dashi bukur"
+    }
+    else if (komputer ==2 && secim ==1) {
+        netice = "Uduzdun! dash qaycini sindirir!"
+    }
+    else if(komputer ==2 && secim ==2) {
+        netice = "Yeniden cehd edin"
+    }
+
+    neticeniYaz.innerHTML = netice
 }
 
 function RandomSecim() {
     const say = Math.round(Math.random()*2) // 0-2 arasindaki ededleri alariq
     komputerinSecimi.src = `img/${siyahi[say]}` // template literal
-   
+
+    return say
+   // ${}
 
 }
